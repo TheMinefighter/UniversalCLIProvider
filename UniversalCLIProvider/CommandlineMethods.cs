e783be11aff61a6f68f3b0aecd7341edd7f4ba9b
Tobias Brohl aka The_Minefighter <tobrohl@gmail.com>
2018-12-05 21:15:36 +0100

    Removing stuff which has been externalized

--- UniversalCLIProvider/CommandlineMethods.cs
+++ UniversalCLIProvider/CommandlineMethods.cs
@@ -3,7 +3,6 @@ using System.CodeDom;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
@@ -104,19 +103,6 @@ public static class CommandlineMethods {
 		}
 
 		return true;
-	}
-
-	public static TypeInfo GetTypeInfo(MemberInfo member) {
-		switch (member) {
-			case PropertyInfo propertyInfo:
-				propertyInfo.PropertyType.GetTypeInfo();
-				break;
-			case FieldInfo fieldInfo:
-				fieldInfo.FieldType.GetTypeInfo();
-				break;
-		}
-
-		throw new ArgumentOutOfRangeException(nameof(member), member, "Must be  or FieldInfo");
 	}
 
 	public static bool ArgumentsFromHex(string arg, out List<string> newArgs) {
