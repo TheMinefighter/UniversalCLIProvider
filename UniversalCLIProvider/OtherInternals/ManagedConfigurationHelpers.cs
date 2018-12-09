a221698d64e496988aa1a2e1bbd5e0d236acb906
Tobias Brohl aka The_Minefighter <tobrohl@gmail.com>
2018-12-09 22:16:14 +0100

    This method is long

--- UniversalCLIProvider/OtherInternals/ManagedConfigurationHelpers.cs
+++ UniversalCLIProvider/OtherInternals/ManagedConfigurationHelpers.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using PropertyOrFieldInfoPackage;
 using System.Linq;
 using System.Reflection;
@@ -8,19 +9,77 @@ using UniversalCLIProvider.OtherInternals;
 
 namespace UniversalCLIProvider.OtherInternals {
 public static class ManagedConfigurationHelpers {
-	public static bool ResolvePath(string path, object item, PropertyOrFieldInfo prop) {
+	public static bool ResolvePath(string path, object item, out PropertyInfo prop, out object[] requiredIndexers,
+		out object newObject) {
+		prop = null;
+		newObject = null;
+		requiredIndexers = null;
 		path = path.Trim();
+		TypeInfo typeInfoOfItem = item.GetType().GetTypeInfo();
 		if (path.StartsWith("[")) {
-				
+			int endOfIndexer = path.IndexOf(']');
+			if (endOfIndexer == -1) {
+				return false;
+			}
+
+			if (!SplitIndexerArguments(path.Substring(1, endOfIndexer - 2), out string[] indexerParameters)) {
+				return false;
+			}
+
+			if (!ResolveIndexerParameters(indexerParameters, typeInfoOfItem, out requiredIndexers, out prop)) {
+				return false;
+			}
+
+			if (path.Length < endOfIndexer + 1) {
+				object newItem;
+				try {
+					newItem = prop.GetValue(item, requiredIndexers);
+				}
+				catch (Exception) {
+					return false;
+				}
+
+				if (!ResolvePath(path.Substring(endOfIndexer + 1), newItem, out prop, out requiredIndexers, out newObject)) {
+					return false;
+				}
+			}
+
+			return true;
 		}
 
-		return false;
+		string currentPath=path;
+		int dotIndex = path.IndexOf('.');
+		if (dotIndex!=-1) {
+			currentPath = path.Substring(0, dotIndex);
+		}
+
+		prop = typeInfoOfItem.GetUnderlyingTypes().SelectMany(x => x.DeclaredProperties)
+			.FirstOrDefault(x => x.Name.Equals(currentPath, StringComparison.OrdinalIgnoreCase));
+		if (prop is null) {
+			return false;
+		}
+		if (dotIndex!=-1) {
+			object newItem;
+			try {
+				newItem = prop.GetValue(item);
+			}
+			catch (Exception) {
+				return false;
+			}
+
+			if (!ResolvePath(path.Substring(dotIndex + 1), newItem, out prop, out requiredIndexers, out newObject)) {
+				return false;
+			}
+		}
+		return true;
 	}
 
-	public static bool ResolveIndexerParameters(string[] parameters, TypeInfo type,out object[] indexParameters,out PropertyInfo indexer) {
+	public static bool ResolveIndexerParameters(string[] parameters, TypeInfo type, out object[] indexParameters,
+		out PropertyInfo indexer) {
 		indexParameters = new object[parameters.Length];
 		indexer = null;
-		foreach (PropertyInfo possibleIndexer in type.GetUnderlyingTypes().SelectMany(x=>x.DeclaredProperties).Where(x=>x.GetIndexParameters().Length==parameters.Length)) {
+		foreach (PropertyInfo possibleIndexer in type.GetUnderlyingTypes().SelectMany(x => x.DeclaredProperties)
+			.Where(x => x.GetIndexParameters().Length == parameters.Length)) {
 			bool success = true;
 			ParameterInfo[] paras = possibleIndexer.GetIndexParameters();
 			for (int i = 0; i < parameters.Length; i++) {
@@ -40,31 +99,24 @@ public static class ManagedConfigurationHelpers {
 
 		return false;
 	}
-	
-	public static bool SplitIndexerArguments(string src,out string[] result) {
+
+	public static bool SplitIndexerArguments(string src, out string[] result) {
 		if (!src.Contains(',')) {
 			result = new[] {src};
 			return true;
 		}
+
 		//TODO Implement
 		throw new NotImplementedException("Multiple Indexer parameters haven't been implemented yet");
 	}
 
 	public static IEnumerable<TypeInfo> GetUnderlyingTypes(this TypeInfo src) {
-		foreach (Type implementedInterface in src.ImplementedInterfaces) {
-			TypeInfo underlyingTypes = implementedInterface.GetTypeInfo();
-			yield return underlyingTypes;
-			foreach (TypeInfo t in underlyingTypes.GetUnderlyingTypes()) {
-				yield return t;
-			}
+		IEnumerable<TypeInfo> baseEnumerator = src.ImplementedInterfaces.SelectMany(x => x.GetTypeInfo().GetUnderlyingTypes()).Append(src);
+		if (!(src.BaseType is null)) {
+			baseEnumerator = baseEnumerator.Concat(src.BaseType.GetTypeInfo().GetUnderlyingTypes());
 		}
 
-		if (src.BaseType is Type) {
-			foreach (TypeInfo typeInfo in src.BaseType.GetTypeInfo().GetUnderlyingTypes()) {
-				yield return typeInfo;
-			}
-		}
-		
+		return baseEnumerator;
 	}
 }
 }
