86dd423e1444df6efbf88c97a47a6ebed3c6bc42
Tobias Brohl aka The_Minefighter <tobrohl@gmail.com>
2019-01-15 11:02:18 +0100

    Documentation

--- UniversalCLIProvider/Internals/HelpGenerators.Configuration.cs
+++ UniversalCLIProvider/Internals/HelpGenerators.Configuration.cs
@@ -12,20 +12,30 @@ public static partial class HelpGenerators {
 	/// <param name="interpreter">The interpreter to use</param>
 	/// <param name="showGenericConfigurationInfo"></param>
 	public static void PrintConfigurationContextHelp(CmdConfigurationNamespaceAttribute namespaceAttribute, BaseInterpreter interpreter,
-		bool showGenericConfigurationInfo = false) =>
+		bool showGenericConfigurationInfo = false) {
+		if (showGenericConfigurationInfo) {
+			ConfigurationGenericHelp(interpreter, Console.WindowWidth, interpreter.TopInterpreter.Options.DefaultIndent);
+		}
 		ConfigurationNamespaceHelp(namespaceAttribute, Console.WindowWidth, interpreter.TopInterpreter.Options.DefaultIndent);
-
-	public static void ConfigurationNamespaceHelp(CmdConfigurationNamespaceAttribute alias, int width, int indent = 3, TextWriter tw = null) {
+	}
+/// <summary>
+/// Prints help for a configurationNamespace
+/// </summary>
+/// <param name="namespaceAttribute">The configurationNamespace to print help for</param>
+/// <param name="width">The width of the console</param>
+/// <param name="indent">The indent to use</param>
+/// <param name="tw">The Textwriter to write the help to</param>
+	public static void ConfigurationNamespaceHelp(CmdConfigurationNamespaceAttribute namespaceAttribute, int width, int indent = 3, TextWriter tw = null) {
 		tw = tw ?? Console.Out;
-		tw.Write(CommandlineMethods.PadCentered($"{alias.Name}{(alias.IsReadonly ? " (RW)" : "")}", width));
-		if (!(alias.LongDescription is null)) {
-			foreach (string s in alias.LongDescription) {
+		tw.Write(CommandlineMethods.PadCentered($"{namespaceAttribute.Name}{(namespaceAttribute.IsReadonly ? " (RW)" : "")}", width));
+		if (!(namespaceAttribute.LongDescription is null)) {
+			foreach (string s in namespaceAttribute.LongDescription) {
 				CommandlineMethods.PrintWithPotentialIndent(s, width, 0, tw);
 			}
 		}
 
 		tw.Write(CommandlineMethods.PadCentered("Values", width));
-		foreach (CmdConfigurationFieldAttribute valueAttribute in alias.ConfigurationFields) {
+		foreach (CmdConfigurationFieldAttribute valueAttribute in namespaceAttribute.ConfigurationFields) {
 			if (valueAttribute.Description is null) {
 				tw.WriteLine(valueAttribute.Name);
 			}
@@ -37,15 +47,25 @@ public static partial class HelpGenerators {
 	}
 
 	/// <summary>
-	///  Prints help for an fieldAttribute
+	///  Prints help for a configurationField
 	/// </summary>
-	/// <param name="fieldAttribute">The fieldAttribute to print help for</param>
+	/// <param name="fieldAttribute">The configurationField to print help for</param>
 	/// <param name="interpreter">The interpreter to use</param>
 	/// <param name="showGenericConfigurationInfo"></param>
 	public static void PrintConfigurationFieldHelp(CmdConfigurationFieldAttribute fieldAttribute, BaseInterpreter interpreter,
-		bool showGenericConfigurationInfo = false) =>
+		bool showGenericConfigurationInfo = false) {
+		if (showGenericConfigurationInfo) {
+			ConfigurationGenericHelp(interpreter, Console.WindowWidth, interpreter.TopInterpreter.Options.DefaultIndent);
+		}
 		ConfigurationFieldHelp(fieldAttribute, Console.WindowWidth, interpreter.TopInterpreter.Options.DefaultIndent);
-
+	}
+/// <summary>
+/// Prints help about a certain configuration field
+/// </summary>
+/// <param name="fieldAttribute">The field attribute to print help for</param>
+/// <param name="width">The width of the console</param>
+/// <param name="indent">The indent to use</param>
+/// <param name="tw">The Textwriter to write the help to</param>
 	public static void ConfigurationFieldHelp(CmdConfigurationFieldAttribute fieldAttribute, int width, int indent = 3, TextWriter tw = null) {
 		tw = tw ?? Console.Out;
 		tw.Write(CommandlineMethods.PadCentered(
@@ -64,17 +84,24 @@ public static partial class HelpGenerators {
 			}
 		}
 	}
-
-	public static void ConfigurationGenericHelp(int width, BaseInterpreter interpreter, int indent = 3, TextWriter tw = null) {
+/// <summary>
+/// Prints generic help about the configuration
+/// </summary>
+/// <param name="interpreter">The current interpreter</param>
+/// <param name="width">The width of the console</param>
+/// <param name="indent">The indent to use</param>
+/// <param name="tw">The Textwriter to write the help to</param>
+	public static void ConfigurationGenericHelp(BaseInterpreter interpreter, int width, int indent = 3, TextWriter tw = null) {
 		tw = tw ?? Console.Out;
+		string indentString = new string(' ',indent);
 		tw.Write(CommandlineMethods.PadCentered($"Usage of {interpreter.Name}", width));
 		tw.WriteLine($"{interpreter.Path} NameOfValue help");
-		string indentString = new string(' ',indent);
 		tw.WriteLine(indentString+ "Prints help for the field");
 		tw.WriteLine($"{interpreter.Path} NameOfValue get");
 		tw.WriteLine(indentString+ "Reads the field");
 		tw.WriteLine($"{interpreter.Path} NameOfValue set NewValue");
 		tw.WriteLine(indentString+ "Sets the field to the value provided");
+		//TODO Add add&remove when implemented
 	}
 }
 }
