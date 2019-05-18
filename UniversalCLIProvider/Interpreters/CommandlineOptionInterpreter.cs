451d5ac17cdb6ab07dbf73a7d3064647e5d1de49
Tobias Brohl <tobrohl@gmail.com>
2019-05-18 08:45:34 +0200

    make more docs

--- UniversalCLIProvider/Interpreters/CommandlineOptionInterpreter.cs
+++ UniversalCLIProvider/Interpreters/CommandlineOptionInterpreter.cs
@@ -12,6 +12,11 @@ namespace UniversalCLIProvider.Interpreters {
 /// </summary>
 public class CommandlineOptionInterpreter {
 	/// <summary>
+	/// The options to be used for interpretation tasks run by this Interpreter
+	/// </summary>
+	public readonly InterpretingOptions Options;
+
+	/// <summary>
 	/// The <see cref="CmdContextAttribute"/> of the class that is used as initial context
 	/// </summary>
 	private CmdContextAttribute _topContextAttribute;
@@ -20,8 +25,6 @@ public class CommandlineOptionInterpreter {
 	/// The arguments to be interpreted
 	/// </summary>
 	public string[] Args;
-
-	public InterpretingOptions Options;
 
 	public CommandlineOptionInterpreter(string[] args, InterpretingOptions options = null) {
 		Args = args;
@@ -34,14 +37,22 @@ public class CommandlineOptionInterpreter {
 	/// <param name="args">The arguments to interpret</param>
 	/// <param name="options">optional options for interpretation</param>
 	/// <typeparam name="T">The type to use as initial context</typeparam>
+	/// <remarks>
+	/// <code>CommandlineOptionInterpreter.Interpret&lt;T&gt;(args,options);</code>
+	/// is a shortcut of
+	/// <code>new CommandlineOptionInterpreter(args,options).Interpret(typeof(T));</code>
+	/// </remarks>
 	[UsedImplicitly]
 	public static void Interpret<T>(string[] args, InterpretingOptions options = null) =>
 		new CommandlineOptionInterpreter(args, options).Interpret(typeof(T));
 
-	public void Interpret<T>() {
-		Interpret(typeof(T));
-	}
 
+	/// <summary>
+	/// Starts the Interpretation of commandline arguments
+	/// </summary>
+	/// <param name="baseContext">The type which shall be used as initial context, which shall have a CmdContextAttribute</param>
+	/// <exception cref="InvalidCLIConfigurationException">Thrown if the CLI is not defined according to the specification</exception>
+	[UsedImplicitly]
 	public void Interpret(Type baseContext) {
 		_topContextAttribute = baseContext.GetCustomAttribute(typeof(CmdContextAttribute)) as CmdContextAttribute;
 		if (_topContextAttribute is null) {
